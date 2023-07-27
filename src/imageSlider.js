7455645e8a44e2c629ff073d1673f2200458bbc2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-27 13:46:01 +0100

    Move Slide Container Left And Right
    Required in order to animate the movement of the image slider.

--- src/imageSlider.js
+++ src/imageSlider.js
@@ -1,7 +1,8 @@
 export default function createImageSlider() {
 
-    function createElement(elType, parentElement, ...classes) {
+    function createElement(elType, textContent, parentElement, ...classes) {
         const newEl = document.createElement(elType);
+        newEl.textContent = textContent;
         classes.map(className => newEl.classList.add(className)); 
         parentElement.appendChild(newEl);
         return newEl;
@@ -9,12 +10,12 @@ export default function createImageSlider() {
 
     function createSlideContainer() {
         const body = document.querySelector('body');
-        const pictureFrame = createElement('div', body, 'picture-frame');
-        const wideDiv = createElement('div', pictureFrame, 'wide-div');
+        const pictureFrame = createElement('div', '', body, 'picture-frame');
+        const wideDiv = createElement('div', '', pictureFrame, 'wide-div');
 
-        const slide1 = createElement('div', wideDiv, 'slide', 'slide1');
-        const slide2 = createElement('div', wideDiv, 'slide', 'slide2');
-        const slide3 = createElement('div', wideDiv, 'slide', 'slide3');
+        const slide1 = createElement('div', 'slide 1', wideDiv, 'slide', 'slide1');
+        const slide2 = createElement('div', 'slide 2', wideDiv, 'slide', 'slide2');
+        const slide3 = createElement('div', 'slide 3', wideDiv, 'slide', 'slide3');
     }
 
     createSlideContainer();
@@ -31,6 +32,30 @@ export default function createImageSlider() {
         return document.querySelector('.move-left');
     }
 
+    function checkExistingClass(el, className) {
+        return (el.classList.contains(className))
+    }
+
+    function removeExistingClass(el, className) {
+        el.classList.remove(className)
+    }
+
+    function containerMoveLeft() {
+        if (checkExistingClass(getSlideContainer(), 'move-right')) {
+            removeExistingClass(getSlideContainer(), 'move-right')
+        }
+
+        getSlideContainer().classList.add('move-left');
+    }
+
+    function containerMoveRight() {
+        if (checkExistingClass(getSlideContainer(), 'move-left')) {
+            removeExistingClass(getSlideContainer(), 'move-left')
+        }
+
+        getSlideContainer().classList.add('move-right');
+    }
+
     function moveLeft() {
         const firstSlide = getSlideContainer().firstChild;
         getSlideContainer().removeChild(firstSlide);
@@ -45,7 +70,8 @@ export default function createImageSlider() {
     }
 
     
-    getMoveRight().addEventListener('click', moveRight);
-    getMoveLeft().addEventListener('click', moveLeft);
-    // moveRight();
+    // getMoveRight().addEventListener('click', moveRight);
+    // getMoveLeft().addEventListener('click', moveLeft);
+    getMoveRight().addEventListener('click', containerMoveRight);
+    getMoveLeft().addEventListener('click', containerMoveLeft);
 }
